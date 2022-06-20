eb4945bbd7f8c887f153ebaf5b5151737d1d8fb4
Mikhail Viaryha <mishutkavlesu@gmail.com>
2022-06-20 11:07:21 +0300

    feat: implement solution

--- cache.go
+++ cache.go
@@ -1,23 +1,44 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	dict  map[string]string
+	times map[string]time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		dict:  make(map[string]string),
+		times: make(map[string]time.Time),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	deadline, isDeadlineFound := c.times[key]
+	if deadline.Before(time.Now()) && isDeadlineFound {
+		delete(c.dict, key)
+		delete(c.times, key)
+	}
+	value, isFound := c.dict[key]
+	return value, isFound
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.dict[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0, len(c.dict))
+	for k := range c.dict {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.dict[key] = value
+	c.times[key] = deadline
 }
